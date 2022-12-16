6a21426a9a2ea78d35391af8455ea52163cfb830
Alejadnro Aparicio <torasemida@gmail.com>
2022-12-16 18:34:16 +0100

    first commit

--- UT3_exercici.py
+++ UT3_exercici.py
@@ -7,9 +7,11 @@ app.config['SECRET_KEY'] = 'mysecretkey'
 
 @app.route('/', methods = ['POST', 'GET'])
 def index():
+    #Si arriben dades per post
     if request.method == 'POST':
         #Comprova que la data s'hagui inserit
         data = request.form['data']
+        #Es comprova que el camp de la data no estigui buit
         if data =="":
             flash('No está la data definida', 'error')
             return redirect(url_for('index')) 
@@ -18,13 +20,16 @@ def index():
             nom = request.form['nom']
             instalacio = request.form['instalacio']        
             nom = int(nom)
+            #La data es descomposa per treure el dia, el mes i l'any
             dataD = data.split("-")
             dia = dataD[2]
             mes = dataD[1]
-            anyo = dataD[0]            
+            anyo = dataD[0]     
+            #Es torna composar amb la forma correcta per enmagazenar a la base de dades       
             dt = datetime.strptime(
             dia + '/' + mes +'/' + anyo[2] + anyo[3] +' ' + hora, "%d/%m/%y %H:%M")
             formatted_date = dt.strftime('%Y-%m-%d %H:%M:%S')
+            #S'enmagazena a quina setmana pertany
             setmana = dt.isocalendar().week
             diaSetmana = dt.weekday()
 
@@ -32,7 +37,6 @@ def index():
         if diaSetmana == 5 or diaSetmana == 6:            
             flash('Els caps de setmana no hi ha reserves', 'error')
             return redirect(url_for('index')) 
-
 
         else: 
             #Es comprova que no existeix la reserva 
@@ -45,28 +49,27 @@ def index():
             novaReserva=reserva.guardaReserva(formatted_date, nom, instalacio, setmana)            
             flash('Reserva afegida de forma satisfactoria', 'exit')
             return redirect(url_for('index', seccio="reserves"))     
-    
+    #Si les dades arriben per get
     else:  
-
         avui = datetime.now()  
         seccio = request.args.get("seccio") 
+        #Si seccio esta definit determinará a quina pestanya es situarà
         if seccio is None:
             seccio = ""
         else:
             seccio = request.args.get("seccio") 
-
+        #Si la setmana esta definida mostrara la setmana pertinent
         setm = request.args.get("setmana") 
         if setm is None:
             setm = avui.isocalendar().week
         else:
             setm = request.args.get("setmana")                      
          
-        
+        #Carrega els usuaris i les dades de les reserves segons la setmana
         llistaUsuaris=usuari.carregaUsuaris()   
-        llistaReserves=reserva.carregaReservesPerSetmana(int(setm)) 
-        
-        taula=[]
-	
+        llistaReserves=reserva.carregaReservesPerSetmana(int(setm))         
+        #Es crea un diccionari amb les dades de les reserves segons la setmana
+        taula=[]	
         for fila in range(0,5):
             filaTemp=[]
             for columna in range(0,6):
@@ -88,18 +91,22 @@ def index():
 
 @app.route('/nou-usuari', methods = ['POST', 'GET'])
 def nouUsuari():
+    #Si les dades arriben per post es que s'ha onplit un formulari previament
     if request.method == 'POST':
         nom = request.form['nom']
         llinatges = request.form['llinatges']
         telefon = request.form['telefon']
+        #S'enmagazena la informació del nou usuari a la base de dades
         nouUsuari=usuari.guardaUsuari(nom,llinatges,telefon)        
         flash('Nou usuari afegit corretament', 'exit')
-        return redirect(url_for('index', seccio="usuaris"))  
-        return redirect(url_for('index'))
+        #Redirigeix a la pàgina inicial amb un missatge flash
+        return redirect(url_for('index', seccio="usuaris"))         
     
     else:
+        # Si les dades arriben per GET mostra el formulari
         return render_template('nou-usuari.html')
 
+#Rutes per borrar, editar i actualitzar l'usuari
 @app.route('/del-usuari')
 def deleteUsuari():
     idusuari= request.args.get('id_u')	
